2147a7ced8ffe57cfed7d5f4bf827c8fd3ae8bc1
“Kaustavi <kaustavisen8@gmail.com>
2020-05-29 12:48:01 +0530

    Interface

---
+++ ICompanyEmpwageUC11.java
@@ -0,0 +1,5 @@
+public interface ICompanyEmpwageUC11{
+	public void addCompanyEmpWage(String company_name,int emp_rate_per_hour,int working_days,int max_hours_per_month);
+	public void computeEmpwage();
+
+}
